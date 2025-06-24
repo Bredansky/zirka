9ae4e86ff3d6b2b513508e0f14958dd486d93992
Kyrylo Bredansky <reddyhighdef@gmail.com>
2025-06-24 15:39:36 +0200

    Prettier config stays untyped

--- prettier/index.ts
+++ prettier/index.ts
@@ -1,6 +1,7 @@
-import { type Config } from "prettier";
+// Can't be properly typed due to https://prettier.io/docs/configuration#typescript-configuration-files
 
-const config: Config = {
+// eslint-disable-next-line import/no-default-export -- Prettier config requires default export
+export default {
   endOfLine: "lf",
   tabWidth: 2,
   printWidth: 120,
@@ -9,6 +10,3 @@ const config: Config = {
   plugins: ["prettier-plugin-packagejson"],
   trailingComma: "all",
 };
-
-// eslint-disable-next-line import/no-default-export -- Prettier config requires default export
-export default config;
