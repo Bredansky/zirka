0503dc5b0fd0f8a75dd8d286e265779c22370ff1
Kyrylo Bredansky <reddyhighdef@gmail.com>
2025-09-08 22:22:00 +0200

    Removed extra comment

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,4 @@
 import { RuleSeverity, styleguide } from "./eslint";
 
-// Ensure styleguide is a function with the correct type
 // eslint-disable-next-line import/no-default-export -- ESLint config requires default export
 export default styleguide({ node: RuleSeverity.Error, typescript: RuleSeverity.Error });
