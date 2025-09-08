cbef3c51d42bdec64821ae1bb0f9d131cfa8d368
Kyrylo Bredansky <reddyhighdef@gmail.com>
2025-09-08 23:14:04 +0200

    Removed extra jsxA11y rule

--- eslint/react.ts
+++ eslint/react.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "eslint/config";
 import react from "@eslint-react/eslint-plugin";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
-import { type Linter } from "eslint";
-import { jsxA11yRules } from "./rules/jsx-a11y";
 import { JAVASCRIPT_FILES, TYPESCRIPT_FILES } from "./utils/constants";
 
 // ESLint config for React projects with TypeScript + JSX a11y + hooks best practices
@@ -11,12 +9,12 @@ export const reactConfig = defineConfig([
   {
     // Apply React recommended rules for JavaScript files
     files: JAVASCRIPT_FILES,
-    extends: [react.configs.recommended as unknown as Linter.Config],
+    extends: [react.configs.recommended],
   },
   {
     // Apply React + TypeScript recommended rules (with type-checking enabled)
     files: TYPESCRIPT_FILES,
-    extends: [react.configs["recommended-type-checked"] as unknown as Linter.Config],
+    extends: [react.configs["recommended-type-checked"]],
   },
   {
     // Extend strict accessibility rules and latest React Hooks best practices
@@ -24,8 +22,5 @@ export const reactConfig = defineConfig([
       jsxA11y.flatConfigs.strict, // Enforces strong accessibility standards
       reactHooks.configs["recommended-latest"], // Ensures proper usage of React Hooks
     ],
-    rules: {
-      ...jsxA11yRules, // Our custom accessibility rules
-    },
   },
 ]);
--- eslint/rules/jsx-a11y.ts
+++
@@ -1,6 +0,0 @@
-import { type Linter } from "eslint";
-
-export const jsxA11yRules: Linter.RulesRecord = {
-  // This rule has been deprecated, but not yet removed.
-  "jsx-a11y/no-onchange": "off",
-};
