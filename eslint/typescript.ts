18d864f68dfb50d0d6622dcd1c86b5164b90bedb
Kyrylo Bredansky <reddyhighdef@gmail.com>
2025-09-08 22:28:49 +0200

    Fixed typescript import resolver

--- eslint/typescript.ts
+++ eslint/typescript.ts
@@ -6,19 +6,32 @@ import { typescriptExtensionRules } from "./rules/typescript/extenstion";
 import { JAVASCRIPT_FILES, TYPESCRIPT_FILES } from "./utils/constants";
 
 export const typescriptConfig = tseslint.config([
+  // These rules enforce strong type safety and identify potential type-related issues.
   tseslint.configs.strictTypeChecked,
+  // These rules focus on code style and readability specifically for TypeScript.
   tseslint.configs.stylisticTypeChecked,
   {
+    // This thing is needed to allow TS to group the imports correctly
+    settings: {
+      "import/resolver": {
+        typescript: true,
+      },
+    },
+    // This configuration applies specifically to TypeScript files.
     files: TYPESCRIPT_FILES,
     languageOptions: {
+      // Set the parser to tseslint.parser, which is the TypeScript parser for ESLint.
       parser: tseslint.parser,
       parserOptions: {
+        // This allows ESLint to leverage TypeScript's language service for better understanding of the project.
         projectService: true,
       },
     },
+    // Register the TypeScript ESLint plugin for use in this configuration scope.
     plugins: {
       "@typescript-eslint": tseslint.plugin,
     },
+    // Apply custom TypeScript rules.
     rules: {
       ...typescriptRules,
       ...typescriptImportRules,
@@ -26,9 +39,12 @@ export const typescriptConfig = tseslint.config([
     },
   },
   {
+    // This effectively disables all type-checking rules for JavaScript files,
+    // as type-checking is only relevant for TypeScript.
     files: JAVASCRIPT_FILES,
     extends: [tseslint.configs.disableTypeChecked],
   },
-  // They purposely do not seek compatibility with @types/eslint so have to manually assert to avoid extra magic lines
+  // TSESLint purposely do not seek compatibility with @types/eslint,
+  // so a manual type assertion to `Linter.Config[]` is used to avoid type errors
   // https://github.com/typescript-eslint/typescript-eslint/issues/8613
 ]) as Linter.Config[];
