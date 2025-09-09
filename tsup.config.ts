35077305981805b83f7392ed48801ef4b857f179
Kyrylo Bredansky <reddyhighdef@gmail.com>
2025-09-09 15:09:11 +0200

    Support CJS too

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 // eslint-disable-next-line import/no-default-export -- TSUp config requires default export
 export default defineConfig({
   entry: ["styleguide.ts"], // adjust if your main TS file is elsewhere
-  format: ["esm"],
+  format: ["esm", "cjs"], // dual export
   outDir: "dist",
   clean: true, // remove old files in dist
   dts: true, // generate TypeScript declaration files
